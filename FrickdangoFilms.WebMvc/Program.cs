8523fdcea54df77cfdad3e2e9e51484dcc195ad2
Austin Frick <austin.frick429@gmail.com>
2024-08-13 20:24:04 -0400

    program.cs services added to scopped

--- FrickdangoFilms.WebMvc/Program.cs
+++ FrickdangoFilms.WebMvc/Program.cs
@@ -1,4 +1,8 @@
 using FrickdangoFilms.Data;
+using FrickdangoFilms.Services.Genre;
+using FrickdangoFilms.Services.Movie;
+using FrickdangoFilms.Services.MPAA_Rating;
+using FrickdangoFilms.Services.Theater;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +10,10 @@ builder.Services.AddDbContext<FrickdangoFilmsDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))) ;
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddScoped<IMovieService, MovieService>(); 
+builder.Services.AddScoped<IGenreService, GenreService>(); 
+builder.Services.AddScoped<ITheaterService, TheaterService>();
+builder.Services.AddScoped<IMPAA_RatingService, MPAA_RatingService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
